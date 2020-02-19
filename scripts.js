0984d321ee0d24efc0e5ab311d21fd19807186a7
greenTreeData <greentreedatasolutions@gmail.com>
2020-02-19 16:17:51 +0100

    redifined NavBar && acordions && JSON read

--- scripts.js
+++ scripts.js
@@ -1,7 +1,32 @@
 
+function acordion(id){
+    //var xmlhttp = new XMLHttpRequest();
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } 
+    else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
 
 function initJS(){
     console.log("init JS");
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function(){
+        if(this.readyState==4 && this.status==200){
+            var myObj = JSON.parse(this.responseText);
+            for(var i = 0; i < myObj.proj.length; ++i){
+                var acorbutt = document.createElement('button');
+                acorbutt.setAttribute("class", "w3-btn w3-black");
+                acorbutt.innerHTML = myObj.proj[i].titulo;
+                document.getElementById('dropzone').appendChild(acorbutt);
+            }
+        }
+    };
+    req.open("GET", "/resources/proj.json", true);
+    req.send();
+    
 }
 
 function scrollProj() {
@@ -19,33 +44,10 @@ function resetNav(){
     document.getElementById('navFaqs').classList.remove('w3-white');
     document.getElementById('navContacte').classList.remove('w3-white');
 }
-function newFocus(id){
-    console.log(id);
-    /*
-    switch(id){
-        case main:
-            console.log('main');
-            resetNav();
-            id.classList.add('w3-white');
-            break;
-        case proj:
-            console.log('proj');
-            resetNav();
-            id.classList.add('w3-white');
-            break;
-        case faqs:
-            console.log('faqs');
-            resetNav();
-            id.classList.add('w3-white');
-            break;
-        case contacte:
-            console.log('contacte');
-            resetNav();
-            id.classList.add('w3-white');
-            break;
-        default:
-            console.log("otro");
-    }
-    */
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
 }
 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
